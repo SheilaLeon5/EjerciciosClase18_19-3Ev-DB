64d724162885b731dcc162c45edfea09c340c611
Sheila <Sheila@DESKTOP-1PIPS59>
2019-05-08 12:19:23 +0100

    ----

--- src/control/BaseDatos.java
+++ src/control/BaseDatos.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
+
 public class BaseDatos {
 	private String host;   // lugar donde se encuentra el servidor
 	private String dbName;
